33d312626c9d622def2c0290c5d7f073a3709a75
zaheerks <zaheerks@gmail.com>
2021-04-09 12:28:53 +0530

    Create st.py

---
+++ st.py
@@ -0,0 +1,20 @@
+#Nirmala stock breakup picking rules
+1.Price above pivot
+2. Above ichimoko cloud
+3. MACD Cross over
+4. 200/44 Moving average crossover ( nearby)
+5.Momentum > 100
+6.MFI >  55
+7.RSI > 60
+8. CCI crossover > 100
+9.Stochastic > 67
+10.Accumulation + Distribution UP
+11. Price and volume trend UP
+12.NR ( 4or 7 ) or 3 white caNDLES
+13. Opening price above or near to yesterday's close.
+
+
+Use CPR 
+1.Trade after 10 A.M
+2.pick stocks which are within 15 % of 200 day average
+3.
