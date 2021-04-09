3d4fcbc31427d0b4bae15ea5cdab6f005d6faa90
zaheerks <zaheerks@gmail.com>
2021-04-09 14:08:31 +0530

    Update st.py

--- st.py
+++ st.py
@@ -14,7 +14,18 @@
 13. Opening price above or near to yesterday's close.
 
 
-Use CPR 
-1.Trade after 10 A.M
-2.pick stocks which are within 15 % of 200 day average
-3.
+1.Use CPR once it crosses TC, use bhavcopy from nse, so that we cant get trapped for pivot pattern.Else post 10 AM once it crosses R1 buy it.
+TC= Top center pivot
+P = pivot
+BC = Bottom center point
+
+TC= (Pivot - BC) + Pivot
+Pivot = (high + low + close)/3
+BC= (high+ low)/2
+2.Once it crosses TC you can buy only if next two candle's are green and sell it at resistance if quick booking else trail with revised SL .
+Candles duration 5 mins 
+3.Once it breaks BC we can exit 
+3.Strictly Trade after 10 A.M
+4.pick stocks which are within 15 % of 200 day average
+5. candles patterns like morning star , bullish engulfing , hammewr shoukld form at bullish reversal and it has high probability.
+6.
